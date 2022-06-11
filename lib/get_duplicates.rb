9196b5d6e931f4a17339d33c1a68ae27e3bffc4f
WRWPhillips <42WPhillips42@gmail.com>
2022-06-10 19:25:21 -0500

    Iterating over rows and appending to output
    -- Column with user_id added
    -- Now is the time for comparison work
    -- Equipped to handle literally every filter at once if necessary
    -- Need to create data normalization controller that can format phone
    numbers, zip codes, and names and emails if there are discrepancies
    there.

--- lib/get_duplicates.rb
+++ lib/get_duplicates.rb
@@ -20,15 +20,31 @@ class GetDups
     def output
         CSV.open("./outputs/#{File.basename(csv_file, ".csv")}_matched_#{Time.now.to_i}.csv", "w",
             :write_headers => true,
-            :headers => headers.unshift("user_id")
+            :headers => headers.unshift("User_id")
         ) do |csv|
-           
+            CSV.foreach(("./#{csv_file}")) do |row|
+                csv << row.unshift("0")
+            end
         end
-        puts filter_term_array
     end
 
     def headers 
         @headers ||= CSV.open("./#{csv_file}", &:readline)
+    end
+
+    def matched_rows
+
+    end
+
+    def header_filters
+        idx_arr = []
+        headers.each_with_index do |x, i|
+            puts x
+            if filter_term_array.include?(x)
+                idx_arr.append(i)
+            end
+        end
+        @header_filters ||= idx_arr
     end
     
     def filter_term_array
