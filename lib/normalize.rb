5c864b6e3a67063e6b135424ef1ea3d3aba97aff
WRWPhillips <42WPhillips42@gmail.com>
2022-06-14 18:03:30 -0500

    added phonelib gem for normalization

--- lib/normalize.rb
+++ lib/normalize.rb
@@ -1,32 +1,12 @@
-# class Normalize
-#     def initialize(filter_term_array, array_of_rows)
-#         @filter_term_array = filter_term_array
-#         @array_of_rows = array_of_rows
-#     end
+class Normalize
+    def initialize(phone)
+        @phone = phone 
+    end
 
-#     def normalize 
-#         data_section.each do |row|
-#             filter_term_array.each do |filter|
-#                 puts row[filter]
-#             end
-#         end
-#     end 
+    def normalize 
+        phone = Phonelib.parse(phone)
+        return phone 
+    end 
 
-#     private
-
-#     attr_reader :array_of_rows
-#     attr_reader :filter_term_array
-
-#     def data_section
-#         @data_section ||= array_of_rows.slice(1, array_of_rows.length - 1)
-#     end
-
-#     def format(str, filter)
-#         case filter 
-#         when "Email", "Email1", "Email2"
-
-#         when "Zip"
-
-#         when ""
-#     end
-# end
+    private 
+end 
